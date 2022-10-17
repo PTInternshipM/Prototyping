8fc1243244af7121dfa3941851beb304db42322a
PeterKrol <peter.krol@mentechinnovation.eu>
2022-10-17 10:09:42 +0200

    Removed flutter_blue package

--- flutter/flutter_prototype/lib/root_view.dart
+++ flutter/flutter_prototype/lib/root_view.dart
@@ -6,7 +6,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_ble_lib_ios_15/flutter_ble_lib.dart' as ble_lib;
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-// import 'package:flutter_blue/flutter_blue.dart' as blue;
 import 'package:flutter_blue_plus/flutter_blue_plus.dart' as blue_plus;
 import 'package:flutter_prototype/cubits/device/device_state.dart';
 import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
